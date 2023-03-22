30a1ff810c64707107e2f5ca94341f3edd65260b
mbugua <mbuguafaimba@gmail.com>
2023-03-22 09:13:05 +0300

    completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,20 @@ class ArticlesController < ApplicationController
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
+
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] = session[:page_views] +1
+
+    if session[:page_views] <= 3
+      render json: article
+    else
+      render json: {error:"Maximum pageview limit reached"}, status: :unauthorized
+    end
   end
 
   private
